a2c8d42d2a3d574dad7bf125a0c7226d48b01c01
sershishkov <ser.shishkov@gmail.com>
2024-03-16 10:56:04 +0200

    F:JkhPismoOtOsbbToPrint

--- app/(front)/manager/refdata/contract/print/jkh-pismo-ot-osbb/[id]/page.tsx
+++ app/(front)/manager/refdata/contract/print/jkh-pismo-ot-osbb/[id]/page.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+'use client';
+import React, { useState, useLayoutEffect } from 'react';
+import { paramsProps } from '@/interfaces/CommonInterfaces';
+import { I_Contract, I_Client } from '@/interfaces/refdata';
+import { item__get_one } from '@/lib/actions/refdata.actions';
 
-export default function JkhPismoOtOsbbPrint() {
-  return <div>JkhPismoOtOsbbPrint</div>;
+import JkhPismoOtOsbbToPrint from '@/components/documents/formsToPrint/jkh/JkhPismoOtOsbbToPrint';
+
+export default function JkhPismoOtOsbbPrint({ params }: Readonly<paramsProps>) {
+  const { id } = params;
+  const [currentContract, setCurrentContract] = useState<I_Contract>();
+  const [currentClient, setCurrentClient] = useState<I_Client>();
+
+  useLayoutEffect(() => {
+    if (id) {
+      const myGetOne = async () => {
+        const localContract = await item__get_one(
+          { _id: id },
+          '/manager/refdata/contract'
+        );
+        const localClient = await item__get_one(
+          //@ts-ignore
+          { _id: localContract?.client._id },
+          '/manager/refdata/client'
+        );
+
+        setCurrentContract(localContract);
+        setCurrentClient(localClient);
+      };
+      myGetOne();
+    }
+  }, [id]);
+  return (
+    <JkhPismoOtOsbbToPrint
+      currentContract={currentContract!}
+      currentClient={currentClient!}
+    />
+  );
 }
