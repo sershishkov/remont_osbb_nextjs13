902354e9d5dc74e4a4bd8b4ae01b59f4908faf07
sershishkov <ser.shishkov@gmail.com>
2024-03-24 10:12:19 +0200

    F:RemsBudjetNakazGipToPrint

--- app/(front)/manager/refdata/contract/print/rems-budjet-nakaz-gip/[id]/page.tsx
+++ app/(front)/manager/refdata/contract/print/rems-budjet-nakaz-gip/[id]/page.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+'use client';
+import React, { useState, useLayoutEffect } from 'react';
+import { paramsProps } from '@/interfaces/CommonInterfaces';
+import { I_Contract, I_Client } from '@/interfaces/refdata';
+import { item__get_one } from '@/lib/actions/refdata.actions';
 
-export default function RemsBudjetNakazGipPrint() {
-  return <div>RemsBudjetNakazGipPrint</div>;
+import RemsBudjetNakazGipToPrint from '@/components/documents/formsToPrint/rems-budjet/RemsBudjetNakazGipToPrint';
+
+export default function RemsBudjetNakazGipPrint({
+  params,
+}: Readonly<paramsProps>) {
+  const { id } = params;
+  const [currentContract, setCurrentContract] = useState<I_Contract>();
+  const [currentExecutor, setCurrentExecutor] = useState<I_Client>();
+
+  useLayoutEffect(() => {
+    if (id) {
+      const myGetOne = async () => {
+        const localContract = await item__get_one(
+          { _id: id },
+          '/manager/refdata/contract'
+        );
+        const localExecutor = await item__get_one(
+          //@ts-ignore
+          { _id: localContract?.ourFirm._id },
+          '/manager/refdata/client'
+        );
+
+        setCurrentContract(localContract);
+        setCurrentExecutor(localExecutor);
+      };
+      myGetOne();
+    }
+  }, [id]);
+
+  return (
+    <RemsBudjetNakazGipToPrint
+      currentContract={currentContract!}
+      currentExecutor={currentExecutor!}
+    />
+  );
 }
